b9ea4253161f8e11ff4e139a5f78dc41fd9e48b7
dimap <dimap@DESKTOP-4IVPGHB>
2023-02-08 19:56:34 +0300

    Добавьте файлы проекта.

---
+++ Figures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converterfailov
+{
+    public class Figures
+    {
+        public string name;
+        public int height;
+        public int width;
+
+        public Figures(string _name, int _h, int _w)
+        {
+            name = _name;
+            height = _h;
+            width = _w;
+        }
+        public Figures()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Converterfailov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите полный путь до файла: ");
+            string pathtofile = Console.ReadLine();
+            readandsave file = new readandsave(pathtofile);
+            file.readFile();
+        }
+    }
+}
